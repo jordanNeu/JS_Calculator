f4c70dafdd10ac1c9db79ccf87edd74eb0ac8016
Jordan Neumann <Jneumann26@gmail.com>
2022-10-23 11:35:13 -0400

    Calculator Working, need to fiddle with design and comment

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let currentOperation = null;
-// Calculator Class
+// Calculator Class Constructor, creates the two objects that will be calculated 
 class Calculator {
     constructor(previousOperandText, currentOperandText) {
         this.previousOperandText = previousOperandText
         this.currentOperandText = currentOperandText
+        this.clear()
     }
-// Clear Function that sets all values to blank
+// Clear Function (AC) that sets all values to blank
 clear() {
-    this.currentOperand= ''
+    this.currentOperand = ''
     this.previousOperand = ''
-    this.operationButtons = undefined
+    this.operation = undefined
 }
 // Deletes a single digit
 delete() {
@@ -28,7 +28,7 @@ appendNumber(number) {
 chooseOperation(operation) {
     if(this.currentOperand === '')
         return
-    if(this.previousOperans !== '') {
+    if(this.previousOperand !== '') {
         this.compute()
     }
     this.operation = operation
@@ -42,45 +42,60 @@ compute() {
     // Converts our strings back to numbers
     const previous = parseFloat(this.previousOperand)
     const current = parseFloat(this.currentOperand)
-    if(!isNaN(previous) || isNaN(current))
-        return
-    switch (this.operation) {
+    if (isNaN(previous) || isNaN(current))
+    return
+    switch(this.operation) {
         case '+':
             computation = previous + current
+            console.log("plus!")
             break
         case '-':
             computation = previous - current
+            console.log("minus!")
             break
-        case '*':
+        case 'X':
             computation = previous * current
+            console.log("TImes!")
             break
         case '÷':
             computation = previous / current
+            console.log("Divide!")
             break
-        default: 
-            return
+        default:
+            console.log("Blank!")
+            return;              
     }
     this.currentOperand = computation
     this.operation = undefined
     this.previousOperand = ''
 }
 
-updateDisplay() {
-    this.currentOperandText.innerHTML = this.currentOperand
-    this.previousOperandText.innerHTML = this.previousOperand
-}
+   updateDisplay() {
+    this.currentOperandText.innerText = this.currentOperand
+    if (this.operation!= null) {
+    this.previousOperandText.innerText = 
+    `${this.previousOperand} ${this.operation}`
+    }
+    else {
+        this.previousOperandText.innerText = ''
+    }
 
+    // this.currentOperandText.innerText = this.currentOperand
+    //     this.previousOperandText.innerText = this.previousOperand
+    
+    }
 }
 const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operator");
 const backButton = document.querySelector(".back");
 const allClearButton = document.querySelector(".ac");
 const equalsButton = document.querySelector(".equals");
-const previousOperandText = document.querySelector('[data-previous-operand]')
-const currentOperandText = document.querySelector('[data-current-operand]')
+const previousOperandText = document.querySelector(".previous-operand")
+const currentOperandText = document.querySelector(".current-operand")
 
 
 const calculator = new Calculator(previousOperandText, currentOperandText)
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
@@ -98,6 +113,7 @@ operationButtons.forEach(button => {
 equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
+    console.log("Equals Pressed!")
 })
 
 allClearButton.addEventListener('click', button => {
